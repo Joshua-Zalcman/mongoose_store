9f08ef337192422c86cb32a10e0817a0786d8030
Josh Z <joshz@Joshs-MacBook-Pro.local>
2021-06-08 19:53:55 -0400

    adds styling

--- models/seed.js
+++ models/seed.js
@@ -1,21 +1,40 @@
 module.exports = [
   {
-    name: 'Beans',
-    description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-    img: 'https://imgur.com/LEHS8h3.png',
-    price: 5,
+    name: 'Rancher Hat',
+    description: 'A cool hat made of leather.',
+    img: 'https://cdn.pixabay.com/photo/2017/09/30/09/29/cowboy-hat-2801582_960_720.png',
+    price: 25,
     qty: 99
   }, {
-    name: 'Bones',
-    description: 'It\'s just a bag of bones.',
-    img: 'https://imgur.com/dalOqwk.png',
-    price: 25,
+    name: 'Sun Hat',
+    description: 'This hat is great for a day at the beach.',
+    img: 'https://cdn.pixabay.com/photo/2016/08/28/11/07/hat-1625676_960_720.png',
+    price: 15,
     qty: 0
   }, {
-    name: 'Bins',
-    description: 'A stack of colorful bins for your beans and bones.',
-    img: 'https://imgur.com/ptWDPO1.png',
-    price: 7000,
+    name: 'Magic Hat',
+    description: 'An odd hat that will make your friends uncomfortable.',
+    img: 'https://cdn.pixabay.com/photo/2019/01/03/16/11/cylinder-3911269_960_720.png',
+    price: 30,
     qty: 1
+  }, {
+    name: 'Cowboy Hat',
+    description: 'Do not purchase this hat unless you are cowboy.',
+    img: 'https://cdn.pixabay.com/photo/2017/09/11/13/01/hat-2738831_960_720.png',
+    price: 15,
+    qty: 5
+  }, {
+    name: 'Fancy Hat',
+    description: 'This hat is for all the fancy folks out there.',
+    img: 'https://cdn.pixabay.com/photo/2016/09/16/19/28/hat-1674950_960_720.png',
+    price: 45,
+    qty: 5
+  },
+  , {
+    name: 'Picnic Hat',
+    description: 'This hat will have your acquantences guessing your taste level.',
+    img: 'https://cdn.pixabay.com/photo/2017/04/28/17/46/hat-2268994_960_720.png',
+    price: 5,
+    qty: 65
   }
 ]
